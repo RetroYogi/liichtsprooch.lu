424246a24380e10812aaec306dfde03fea4e5db9
Gérard Kieffer <gerard@mailo.com>
2025-11-09 22:10:24 +0100

    about page

--- about.php
+++ about.php
@@ -15,24 +15,10 @@ $ogType = 'website';
 
 // Include header
 include 'header.php';
-?>
 
-    <!-- Main Container -->
-    <main id="main-content">
-        <section class="content-section" aria-labelledby="about-title">
-            <h1 id="about-title">Iwwer dës Säit</h1>
-            <p>De Site liichtsprooch.lu ass en oppe kollaborative Projet.</p>
+// Include content template
+include 'templates/about-content.php';
 
-<p>Jidderee kann um GitHub-Repository um Projet matmaachen, Artikele verbesseren an nei Artikelen dobäi setzen:
-👉 https://github.com/RetroYogi/liichtsprooch.lu</p>
-
-<p>Den ursprénglechen Inhalt vum Site gouf zum groussen Deel mat kënschtlecher Intelligenz (KI) generéiert an net iwwerpréift.
-
-Doduerch kënne grammatesch oder sproochlech Feeler am Text virkommen. </p>
-        </section>
-    </main>
-
-<?php
 // Include footer
 include 'footer.php';
 ?>
