9ea2e0205688de794062d1845542730bd1c88edd
Gérard Kieffer <gerard@mailo.com>
2025-11-14 18:56:28 +0100

    Updated about page

--- templates/about-content.php
+++ templates/about-content.php
@@ -2,14 +2,7 @@
 <main id="main-content">
     <section class="content-section" aria-labelledby="about-title">
         <h1 id="about-title">Iwwer dës Säit</h1>
-        <p>De Site liichtsprooch.lu ass en oppene kollaborative Projet.</p>
-
-        <p>Jidderee kann iwwer de <a href="https://github.com/RetroYogi/liichtsprooch.lu" target="_blank">GitHub
-                Repository</a> vum Projet Deel huelen: Artikele verbesseren an nei Artikelen dobäi setzen.</p>
-
-        <p>Den ursprénglechen Inhalt vum Site gouf zum groussen Deel mat kënschtlecher Intelligenz (KI) generéiert an
-            net iwwerpréift.<br>
-            Doduerch kënne grammatesch oder sproochlech Feeler am Text virkommen. </p>
+<p>De Site liichtsprooch.lu ass en oppene kollaborative Projet. E gouf aus technesche Grënn entwéckelt, fir Workflows ze testen, déi et erlaben, iwwer GitHub Pages, Static Site Generators a KI en Internetsite ze gestallten an ze geréieren.</p> <p>Jidderee kann iwwer de <a href="https://github.com/RetroYogi/liichtsprooch.lu" target="_blank">GitHub-Repository</a> vum Projet Deel huelen: Informatioune verbesseren an nei Artikelen derbäisetzen.</p> <p>Den ursprénglechen Inhalt vum Site gouf zum groussen Deel mat kënschtlecher Intelligenz (KI) generéiert an net iwwerpréift.<br> Duerch dat kënnen am Text grammatesch oder sproochlech Feeler optrieden, wéi och falsch Informatiounen.</p>
 
     </section>
 </main>
