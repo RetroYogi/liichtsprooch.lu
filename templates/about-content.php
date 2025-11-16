67ac32f7ecf6f003a3aaf63a9e0e506099dfdc2c
Gérard Kieffer <60404287+RetroYogi@users.noreply.github.com>
2025-11-16 21:46:16 +0100

    Update text in about-content.php

--- templates/about-content.php
+++ templates/about-content.php
@@ -2,8 +2,8 @@
 <main id="main-content">
     <section class="content-section" aria-labelledby="about-title">
         <h1 id="about-title">Iwwer dës Säit</h1>
-        <p>Den Inhalt vum Site gouf zum groussen Deel mat kënschtlecher Intelligenz (KI) generéiert an net iwwerpréift.<br>
+        <p>Den Inhalt vum Site gouf zum groussen Deel mat kënschtlecher Intelligenz (KI) generéiert an net vollstänneg iwwerpréift.<br>
         Doduerch kënnen am Text grammatesch oder sproochlech Feeler optrieden, wéi och falsch Informatioune stoen.</p>
-        <p>Jidderee kann iwwer de <a href="https://github.com/RetroYogi/liichtsprooch.lu" target="_blank">GitHub-Repository</a> um Projet Deel huelen: Informatioune verbesseren an nei Artikelen derbäisetzen.</p>
+        <p>Jidderee ass wëllkomm iwwer de <a href="https://github.com/RetroYogi/liichtsprooch.lu" target="_blank">GitHub-Repository</a> um Projet Deel huelen: Informatioune verbesseren an nei Artikelen derbäisetzen.</p>
     </section>
 </main>
