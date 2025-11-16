f18ccf34e8ddf738e5ec3f6da1566ef7749bc1e5
Gérard Kieffer <60404287+RetroYogi@users.noreply.github.com>
2025-11-16 21:48:13 +0100

    Fixed typo

--- templates/about-content.php
+++ templates/about-content.php
@@ -4,6 +4,6 @@
         <h1 id="about-title">Iwwer dës Säit</h1>
         <p>Den Inhalt vum Site gouf zum groussen Deel mat kënschtlecher Intelligenz (KI) generéiert an net vollstänneg iwwerpréift.<br>
         Doduerch kënnen am Text grammatesch oder sproochlech Feeler optrieden, wéi och falsch Informatioune stoen.</p>
-        <p>Jidderee ass wëllkomm iwwer de <a href="https://github.com/RetroYogi/liichtsprooch.lu" target="_blank">GitHub-Repository</a> um Projet Deel huelen: Informatioune verbesseren an nei Artikelen derbäisetzen.</p>
+        <p>Jiddereen ass wëllkomm iwwer de <a href="https://github.com/RetroYogi/liichtsprooch.lu" target="_blank">GitHub-Repository</a> um Projet Deel huelen: Informatioune verbesseren an nei Artikelen derbäisetzen.</p>
     </section>
 </main>
